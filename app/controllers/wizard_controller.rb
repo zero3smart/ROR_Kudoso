7e7d377a48673b3818521ababb7d98a9890f362d
zero2smart <fivewisdomzero@gmail.com>
2018-08-16 00:10:32 -0400

    wizard step 3 working

--- app/controllers/wizard_controller.rb
+++ app/controllers/wizard_controller.rb
@@ -2,7 +2,16 @@ class WizardController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    if current_user.wizard_step == 3
+      @family_device_categories = Array.new
+      DeviceCategory.find_each do |device_category|
+        @family_device_categories << { id: device_category.id,
+                                       name: device_category.name,
+                                       description: device_category.description,
+                                       amount: current_user.family.family_device_categories.where(device_category_id: device_category.id).first.try(:amount) || 0 }
+      end
 
+    end
   end
 
   def create
@@ -11,8 +20,34 @@ class WizardController < ApplicationController
     else
       current_user.update_attribute(:wizard_step, 0 )
     end
+
     respond_to do |format|
-      format.js { render :index }
+      format.json { render json: { step: current_user.wizard_step} }
+    end
+
+  end
+
+  def update
+    if params[ "step" ].to_i == 3
+       params[ "device_categories" ].each do |key, value|
+         unless key.ends_with?('_other')
+           id = "#{key}"
+           id.slice!('device_category_')
+           device_category = FamilyDeviceCategory.find_or_create_by(device_category_id: id.to_i, family_id: current_user.family.id)
+           if value.to_i != 4
+             device_category.amount = value.to_i
+           else
+             device_category.amount = params[ "device_categories" ][ "device_category_#{id}_other"].to_i
+           end
+           device_category.save
+           logger.info "Saved device category: #{device_category.inspect}"
+         end
+       end
+
+    end
+
+    respond_to do |format|
+      format.json { render json: { message: "success" } }
     end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -88,7 +88,6 @@ Rails.application.routes.draw do
 
   end
 
-  resources :wizard
 
   get 'tos', to: 'home#tos'
   get 'privacy', to: 'home#privacy'
@@ -101,8 +100,9 @@ Rails.application.routes.draw do
   get 'content_filtering', to: 'home#content_filtering'
   get 'kudos', to: 'home#kudos'
   get 'rewards', to: 'home#rewards'
-  # get 'wizard', to: 'wizard#index'
-  # put 'wizard', to: 'wizard#update'
+  get 'wizard', to: 'wizard#index'
+  put 'wizard', to: 'wizard#update'
+  post 'wizard', to: 'wizard#create'
   root to: 'home#landing'
 
   match "*path", to: "errors#catch_404", via: :all
