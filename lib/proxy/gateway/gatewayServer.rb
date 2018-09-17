fac175fa30b21f70230e218e2eea3f4f5ca38b02
zero2smart <fivewisdomzero@gmail.com>
2018-09-17 00:22:53 -0400

    webrick server now handles file upload with working example

--- lib/proxy/gateway/gatewayServer.rb
+++ lib/proxy/gateway/gatewayServer.rb
@@ -1,6 +1,22 @@
 require 'pry'
 require 'webrick'
+require 'stringio'
+
 include WEBrick
+
+class FileUploadServlet < HTTPServlet::AbstractServlet
+  def do_POST(req, res)
+    filedata = req.query["filename"]
+    if filedata.nil?
+      res.set_redirect HTTPStatus::TemporaryRedirect, "/upload_fail.rhtml"
+    else
+      f = File.open("uploads/firmware.bin", "wb")
+      f.syswrite filedata
+      f.close
+      res.set_redirect HTTPStatus::TemporaryRedirect, "/upload_success.rhtml"
+    end
+  end
+end
 
 class KudosoGWServlet < HTTPServlet::AbstractServlet
   def do_GET(req,resp)
@@ -47,6 +63,9 @@ end
 
 
 server = WEBrick::HTTPServer.new :Port => 1234
-server.mount "/", KudosoGWServlet
+HTTPUtils::DefaultMimeTypes.store('rhtml', 'text/html')
+#server.mount "/", KudosoGWServlet
+server.mount "/",HTTPServlet::FileHandler, "./html"
+server.mount "/upload", FileUploadServlet
 trap('INT') { server.stop }
 server.start
