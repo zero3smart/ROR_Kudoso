d969ca1eddf02171b942cfec4191a980ca2fe97e
zero2smart <fivewisdomzero@gmail.com>
2018-10-12 22:31:52 -0400

    wired up newletter signups

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -58,3 +58,8 @@ $(function () {
 $(document).on("page:load ready", function () {
     $("input.datepicker").datepicker();
 });
+
+function validateEmail(email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+}
