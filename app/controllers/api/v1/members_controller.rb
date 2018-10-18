d55250d82f9d92ae2800df1f976656e41e88d7ee
zero2smart <fivewisdomzero@gmail.com>
2018-10-18 02:02:37 -0400

    make memebrs return for family member login screen

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -13,11 +13,8 @@ module Api
           == API Members
           Once authenticated, you can retrieve Member specific information.
 
-<<<<<<< HEAD
-=======
           Member object returns as JSON with embedded Theme object.
 
->>>>>>> 992a42491dc2ec4b996eb28aaa06b5466fdfeeaa
         EOS
       end
       def_param_group :member do
@@ -26,27 +23,20 @@ module Api
         param :birth_date, String, desc: "The member's birth date in MM/DD/YYYY format"
         param :email, String, desc: "Optionally define an email address for this member (useful for notifications and updates)"
         param :parent, [true, false], desc: "Set to true if this member is a parent in the family (default: false)"
-<<<<<<< HEAD
-      end
-
-      api :GET, "v1/families/:family_id/members", "Retrieve all family members (authenticated user must be a parent)"
-=======
         param :gender, ['m', 'f'], desc: "Gender of member as (m)ale or (f)emale"
       end
 
-      api :GET, "/v1/families/:family_id/members", "Retrieve all family members (authenticated user must be a parent)"
->>>>>>> 992a42491dc2ec4b996eb28aaa06b5466fdfeeaa
+      api :GET, "/v1/families/:family_id/members", "Retrieve all family members (authenticated user must be a parent to retreive all info)"
       example '  [{"id"=>3, "username"=>"suzy", "birth_date"=>Thu, 02 Jul 2009, "parent"=>nil, "family_id"=>1, "contact_id"=>nil, "created_at"=>Thu, 02 Jul 2015 10:15:52 MDT -06:00, "updated_at"=>Thu, 02 Jul 2015 10:16:02 MDT -06:00, "kudos"=>1200, "authentication_token"=>nil, "first_name"=>"Suzy", "last_name"=>"Test", "email"=>nil, "mobicip_profile"=>nil}, {"id"=>1, "username"=>"parent@kudoso.com", "birth_date"=>nil, "parent"=>true, "family_id"=>1, "contact_id"=>nil, "created_at"=>Thu, 02 Jul 2015 10:15:52 MDT -06:00, "updated_at"=>Thu, 02 Jul 2015 10:22:08 MDT -06:00, "kudos"=>0, "authentication_token"=>nil, "first_name"=>"Parent", "last_name"=>"Test", "email"=>"parent@kudoso.com", "mobicip_profile"=>nil}, {"id"=>4, "username"=>"timmy", "birth_date"=>Sat, 08 Mar 2003, "parent"=>false, "family_id"=>1, "contact_id"=>nil, "created_at"=>Thu, 02 Jul 2015 10:22:45 MDT -06:00, "updated_at"=>Thu, 02 Jul 2015 10:22:45 MDT -06:00, "kudos"=>0, "authentication_token"=>nil, "first_name"=>"Timmy", "last_name"=>"Test", "email"=>nil, "mobicip_profile"=>nil}, {"id"=>2, "username"=>"johnny", "birth_date"=>Sat, 02 Jul 2005, "parent"=>nil, "family_id"=>1, "contact_id"=>nil, "created_at"=>Thu, 02 Jul 2015 10:15:52 MDT -06:00, "updated_at"=>Thu, 02 Jul 2015 10:16:01 MDT -06:00, "kudos"=>1420, "authentication_token"=>nil, "first_name"=>"Johnny", "last_name"=>"Test", "email"=>nil, "mobicip_profile"=>nil}]  '
       def index
         messages = init_messages
         begin
           @family = Family.find(params[:family_id])
+          @members = @family.members
           if @current_user && ( @current_user.admin? || @current_user.family == @family)
-            @members = @family.members
             render :json => { :members => @members, :messages => messages }, :status => 200
           else
-            messages[:error] << 'You are not authorized to do this.'
-            render :json => { :messages => messages }, :status => 403
+            render :json => { :members => @members.as_json(only: [:username, :parent, :first_name], include: :theme), :messages => messages }, :status => 200
           end
 
         rescue ActiveRecord::RecordNotFound
@@ -82,11 +72,7 @@ module Api
 
       end
 
-<<<<<<< HEAD
-      api :POST, "v1/families/:family_id/members", "Create a new family member (authenticated user must be a parent)"
-=======
       api :POST, "/v1/families/:family_id/members", "Create a new family member (authenticated user must be a parent)"
->>>>>>> 992a42491dc2ec4b996eb28aaa06b5466fdfeeaa
       param_group :member
       example '  {"id"=>3, "username"=>"suzy", "birth_date"=>Thu, 02 Jul 2009, "parent"=>nil, "family_id"=>1, "contact_id"=>nil, "created_at"=>Thu, 02 Jul 2015 10:15:52 MDT -06:00, "updated_at"=>Thu, 02 Jul 2015 10:16:02 MDT -06:00, "kudos"=>1200, "authentication_token"=>nil, "first_name"=>"Suzy", "last_name"=>"Test", "email"=>nil, "mobicip_profile"=>nil} '
       def create
@@ -130,48 +116,12 @@ module Api
             local_params = member_create_params.merge(family_id: @family.id)
             local_params[:birth_date] = Chronic.parse(local_params[:birth_date]).to_date.to_s(:db) if local_params[:birth_date]
             if local_params[:avatar]
-<<<<<<< HEAD
-              local_params[:avatar] = parse_image_data(local_params[:avatar])
-=======
               @member.avatar = parse_image_data(local_params[:avatar])
               @member.save
               local_params.delete(:avatar)
->>>>>>> 992a42491dc2ec4b996eb28aaa06b5466fdfeeaa
             end
 
             if @member.update_attributes(local_params)
-              render :json => { :member => @member, :messages => messages }, :status => 200
-            else
-              messages[:error] << @member.errors.full_messages
-              render :json => { :member => @member, :messages => messages }, :status => 400
-            end
-
-          else
-            messages[:error] << 'You are not authorized to do this.'
-            render :json => { :messages => messages }, :status => 403
-          end
-
-        rescue ActiveRecord::RecordNotFound
-          messages[:error] << 'Family not found.'
-          render :json => { :messages => messages }, :status => 404
-        rescue
-          messages[:error] << 'A server error occurred.'
-          render :json => { :messages => messages }, :status => 500
-        end
-      ensure
-        clean_tempfile
-
-      end
-
-      api :DELETE, "/v1/families/:family_id/members/:member_id", "Delete a family member (authenticated user must be a parent)"
-      param_group :member
-      def destroy
-        messages = init_messages
-        begin
-          @family = Family.find(params[:family_id])
-          if @current_user.try(:admin) || (@current_member.try(:family) == @family && @current_member.parent )
-            @member = @family.members.find(params[:id])
-            if @member.destroy
               render :json => { :member => @member, :messages => messages }, :status => 200
             else
               messages[:error] << @member.errors.full_messages
@@ -229,35 +179,7 @@ module Api
 
       # Never trust parameters from the scary internet, only allow the white list through.
       def member_create_params
-<<<<<<< HEAD
-        params.require(:member).permit(:username, :parent, :password, :password_confirmation, :birth_date, :first_name, :last_name, :email, :avatar)
-      end
-
-      # http://paoloibarra.com/2014/09/27/Image-Upload-Using-Rails-API-And-Paperclip/
-      def parse_image_data(image_data)
-        logger.info image_data.inspect
-        @tempfile = Tempfile.new("member_image_#{ Time.now.to_i}")
-        @tempfile.binmode
-        @tempfile.write Base64.decode64(image_data[:content])
-        @tempfile.rewind
-
-        uploaded_file = ActionDispatch::Http::UploadedFile.new(
-            tempfile: @tempfile,
-            filename: image_data[:filename]
-        )
-
-        uploaded_file.content_type = image_data[:content_type]
-        uploaded_file
-      end
-
-      def clean_tempfile
-        if @tempfile
-          @tempfile.close
-          @tempfile.unlink
-        end
-=======
         params.require(:member).permit(:username, :parent, :password, :password_confirmation, :birth_date, :first_name, :last_name, :gender, :email, :theme_id, avatar: %w(content-type content))
->>>>>>> 992a42491dc2ec4b996eb28aaa06b5466fdfeeaa
       end
 
 
