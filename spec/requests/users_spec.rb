e31c9f5b39b56cc61957dd7a9e3b04c4c263ad24
zero2smart <fivewisdomzero@gmail.com>
2018-11-19 00:16:12 -0500

    fix broken test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Users", :type => :request do
-  describe "GET /users/sign_up" do
-    it 'retruns a form with the required fields' do
-      get '/users/sign_up'
+  describe "GET /users/sign_in" do
+    it 'returns a form with the required fields' do
+      get '/users/sign_in'
       expect(response.status).to eq(200)
     end
   end
